3a2881ae3ba452379c035ba0455334268b21d161
DAHCROWN <57120458+DAHCROWN@users.noreply.github.com>
2023-12-08 02:06:20 +0100

    attempt to fix get error

--- pages/api/meters.js
+++ pages/api/meters.js
@@ -2,52 +2,51 @@ import db_query from "@/helpers/db";
 import { upload_data } from "@/helpers/db";
 import { connectDB } from "@/lib/mongodb";
 import UsageProperties from "@/models/powerUsage";
-import { revalidatePath } from 'next/cache'
+import { revalidatePath } from 'next/cache';
 
 export default async function handler(req, res) {
-try {
-  connectDB().catch((err) => res.json(err));
-  if (req.method == "GET") {
-    const { id } = req.query;
-    const meterUsage = await UsageProperties.findOne({ id: id})
-    if (!meterUsage){
-      const data = db_query(id);
-      res.status(200).json({ message: data });
+  try {
+    await connectDB(); // Make sure to await connectDB to ensure it's completed
 
+    if (req.method === "GET") {
+      const { id } = req.query;
+      const meterUsage = await UsageProperties.findOne({ id: id });
+
+      if (!meterUsage) {
+        const data = db_query(id);
+        return res.status(200).json({ message: data });
+      }
+
+      return res.status(200).json({ message: meterUsage });
+    } else if (req.method === "POST") {
+      const { id, gen_current, gen_voltage, con_current, con_voltage } = req.query;
+      const meterUsage = await UsageProperties.findOne({ id: id });
+
+      if (meterUsage) {
+        meterUsage.gen_current = gen_current;
+        meterUsage.gen_voltage = gen_voltage;
+        meterUsage.con_current = con_current;
+        meterUsage.con_voltage = con_voltage;
+
+        await meterUsage.save();
+        // revalidatePath(`/meters}`);
+        return res.status(200).json({ success: true, usage: meterUsage });
+      } else {
+        const createdMeterUsage = await UsageProperties.create({
+          id,
+          gen_current,
+          gen_voltage,
+          con_current,
+          con_voltage,
+        });
+        // revalidatePath(`/meters`);
+        return res.status(201).json({ success: true, usage: createdMeterUsage });
+      }
     }
-    // const data = db_query(id);
-    res.status(200).json({ message: meterUsage });
-    // res.status(200).json({ message: `Hello from Powerboard! AGAIN ${id}` });
-    
 
-
-
-  } else if (req.method == "POST") {
-    const { id, gen_current, gen_voltage, con_current, con_voltage } = req.query;
-    const meterUsage = await UsageProperties.findOne({ id: id})
-    if(meterUsage){
-      meterUsage.gen_current = gen_current
-      meterUsage.gen_voltage = gen_voltage
-      meterUsage.con_current = con_current
-      meterUsage.con_voltage = con_voltage
-      await meterUsage.save();
-      // revalidatePath(`/meters}`)
-      return res.status(200).json({success: true, usage: meterUsage})
-    }else{
-      UsageProperties.create({
-        id,
-        gen_current, gen_voltage, con_current, con_voltage
-      })
-      // revalidatePath(`/meters`)
-      // await meterUsage.save();
-      return res.status(201).json(({success: true, user: meterUsage}))
-    }
-
+    res.status(200).json({ message: "Hello from Next.js!" });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  res.status(200).json({ message: "Hello from Next.js!" });
-} catch (error) {
-  console.error('Error:', error)
-  res.status(500).json({ error: 'Internal Server Error' });
-}
 }
